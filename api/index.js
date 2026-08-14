65bc302a431bc7ed699640f1ec4aec7a0587fd30
Chinmay Gurjar <gurjarchinmay01@gmail.com>
2026-08-14 12:22:47 +0530

    Second

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path");
+const app = require("../server");
+module.exports = app;
 
 dotenv.config();
 
